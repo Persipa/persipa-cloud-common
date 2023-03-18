372a368cf95e2f734af23bc57cc794fd61797504
Persipa <khxu10234@icloud.com>
2023-03-18 15:23:58 +0800

    feature(CloudEntity): 返回增加异常类型

--- src/main/java/site/persipa/cloud/pojo/model/Result.java
+++ src/main/java/site/persipa/cloud/pojo/model/Result.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import site.persipa.cloud.enums.ResultCodeEnum;
+import site.persipa.cloud.exception.PersipaBaseException;
 
 /**
  * @author persipa
@@ -30,6 +31,13 @@ public class Result<T> {
 
     public static Result<Void> fail(ResultCodeEnum resultCodeEnum) {
         return Result.resultWithoutData(resultCodeEnum);
+    }
+
+    public static Result<String> exception(PersipaBaseException exception) {
+        if (exception == null) {
+            return Result.resultWithData(ResultCodeEnum.ERROR, "unknown exception");
+        }
+        return new Result<>(exception.getCode(), exception.getMsg(), exception.getDescription());
     }
 
     private static Result<Void> resultWithoutData(ResultCodeEnum resultCodeEnum) {
