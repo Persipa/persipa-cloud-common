92927491799af723d4c2bb8b4e8e063d1e448e96
Persipa <khxu10234@icloud.com>
2023-05-17 17:23:57 +0800

    feat(exception): 增加通用运行时异常

---
+++ src/main/java/site/persipa/cloud/exception/PersipaRuntimeException.java
@@ -0,0 +1,32 @@
+package site.persipa.cloud.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import site.persipa.cloud.enums.ExceptionLevelEnum;
+
+/**
+ * @author persipa
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class PersipaRuntimeException extends RuntimeException {
+
+    protected int code;
+
+    protected String msg;
+
+    protected String description;
+
+    protected ExceptionLevelEnum level;
+
+    protected PersipaRuntimeException(PersipaBaseExceptionEnum baseExceptionEnum) {
+        this.code = baseExceptionEnum.getCode();
+        this.msg = baseExceptionEnum.getMsg();
+        this.level = baseExceptionEnum.getLevel();
+    }
+
+}
